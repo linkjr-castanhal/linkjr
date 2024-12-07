49494abf4ce691c12174386829c5d17e0fb11578
João Medeiros <joaomanoelmedeirosmatos@gmail.com>
2024-12-06 22:00:26 -0300

    feat diretorios

--- src/app/(servicos)/consultoria-em-TI/page.tsx
+++ src/app/(servicos)/consultoria-em-TI/page.tsx
@@ -1,8 +1,7 @@
 export default function consultoria() {
     return (
       <>
-        consultoria em ti
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, hic corrupti eveniet aliquam provident nihil odio, magnam numquam ea eaque nemo temporibus a minima beatae itaque ratione delectus, est deserunt.
+
       </>
     );
   }
--- src/app/(servicos)/sistemas-embarcados/page.tsx
+++ src/app/(servicos)/sistemas-embarcados/page.tsx
@@ -1,8 +1,6 @@
 export default function sistemas() {
     return (
       <>
-        Sistemas embarcados
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, hic corrupti eveniet aliquam provident nihil odio, magnam numquam ea eaque nemo temporibus a minima beatae itaque ratione delectus, est deserunt.
       </>
     );
   }
