3977c886417e2bb3de1403e0666b3dcbf45be28c
João Medeiros <joaomanoelmedeirosmatos@gmail.com>
2024-12-08 14:39:20 -0300

    feat mudanças

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam tenetur fugit, recusandae minus assumenda quidem earum nostrum officia distinctio ab! Illum, qui! Temporibus voluptatum nulla corrupti. Inventore nihil consequuntur asperiores?</p>
     </div>
   );
 }
