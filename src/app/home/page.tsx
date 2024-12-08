50adbfc7111a7af7b317fc1f7f067379b2b40d8d
João Medeiros <joaomanoelmedeirosmatos@gmail.com>
2024-12-07 21:47:38 -0300

    feat mudancas

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,8 @@
 /* prettier-ignore */
-export function Home(){
-  return(
-  <>
-  <h1>Home</h1>
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione eligendi, odio sapiente quod quam accusamus beatae ab? Numquam architecto ipsam eligendi cum commodi minus soluta asperiores provident? Ducimus, architecto ipsam.</p>
-  </>
-  )
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
 }
