19cb2c5f880db2fa58715f826436ddf30f3c7060
Edson Costa <edson.13.10.2004@gmail.com>
2024-12-08 22:35:18 -0300

    feat: adiciona página Sobre com conteúdo inicial

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,11 @@
 export default function About() {
-    return (
-      <>
-        <h1>About</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
-          eligendi, odio sapiente quod quam accusamus beatae ab architecto ipsam.
-        </p>
-      </>
-    )
-  }
-  
+  return (
+    <>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+        eligendi, odio sapiente quod quam accusamus beatae ab architecto ipsam.
+      </p>
+    </>
+  )
+}
