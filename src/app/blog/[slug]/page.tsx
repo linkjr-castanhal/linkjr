d0034fa7aa1fc581fe42c96618e928017aecdda2
Edson Costa <edson.13.10.2004@gmail.com>
2024-12-08 21:51:46 -0300

    fix: atualiza workflow de CI e melhora formatação de código nas páginas

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,12 +1,11 @@
-// prettier-ignore
 export default function Blog() {
   return (
-      <>
+    <>
       <h1>Blog</h1>
-          <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione eligendi, odio sapiente quod
-              quam accusamus beatae ab architecto ipsam.
-          </p>
-      </>
-  );
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+        eligendi, odio sapiente quod quam accusamus beatae ab architecto ipsam.
+      </p>
+    </>
+  )
 }
