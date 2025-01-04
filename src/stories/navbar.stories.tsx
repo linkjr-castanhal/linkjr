1afe23ea746dc65ed599408b0de7d03c4a3399d6
kwojtyla <karolwojtyla360@gmail.com>
2025-01-04 19:35:36 -0300

    refactor(navbar): update component import in story

--- src/stories/navbar.stories.tsx
+++ src/stories/navbar.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import { Navbar } from '../components/navbar/index'
+import { Navbar } from '../components/navbar'
 
 const meta = {
   component: Navbar,
