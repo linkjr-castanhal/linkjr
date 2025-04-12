2e2df78495c39ebb5b05102f3737aa524cba6c72
kwojtyla <karolwojtyla360@gmail.com>
2025-04-11 22:08:19 -0300

    fix: add secondary variant

--- src/components/shadcn-ui/button.tsx
+++ src/components/shadcn-ui/button.tsx
@@ -11,6 +11,7 @@ const buttonVariants = cva(
       variant: {
         default: 'bg-primary text-white hover:bg-primary-hover',
         primary: 'bg-primary text-white hover:bg-primary-hover',
+        secondary: 'bg-white text-primary hover:bg-white/90',
         outline: 'border border-primary text-primary px-4 py-1 hover:bg-white',
       },
       size: {
